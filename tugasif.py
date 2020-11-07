87830c5c07e5bb1e910e69f9a44c65dcf44746ce
Antini permatasari <antinipermatasari2001@gmail.com>
2020-11-07 09:01:35 +0700

    update folder

--- tugasif.py
+++ tugasif.py
@@ -1,18 +1,22 @@
-print("Masukkan Pilihan Angka ke-1")
-bilangan1 = int(input())
-print("Masukkan Pilihan Angka ke-2")
-bilangan2 = int(input())
-print("Masukkan Pilihan Angka ke-3")
-bilangan3 = int(input())
+# input 3 buah bilangan , menggunakan statement if
+print("menentukan bilangan terbesar dari 3 bilangan")
 
-print("\n")
+print("Masukkan Bilangan ke-1 : ")
+bilangan1=int(input())
+print("Masukkan Bilangan ke-2 : ")
+bilangan2=int(input())
+print("Masukkan Bilangan ke-3 : ")
+bilangan3=int(input())
 
 
-if (bilangan1 > bilangan2) and (bilangan1 > bilangan3):
-    print(f"angka pertama lebih besar dari angka ketiga")
-elif (bilangan2 > bilangan1) and (bilangan2 > bilangan3):
-    print(f"angka dua lebih besar dari angka tiga")
-elif (bilangan3 > bilangan2) and (bilangan3 > bilangan1)
-    print(f"angka tiga lebih besar dari angka satu")
-else:
-    print("angka yang dimasukkan sama besar")
+print('\n')
+
+
+if ( bilangan1 > bilangan2 ) and ( bilangan1 > bilangan3 ) :
+    print("Bilangan 1 lebih Besar dari Bilangan 2 dab 3")
+elif ( bilangan2 > bilangan1) and ( bilangan42 > bilangan3 ) :
+    print ("Bilangan 2 lebih Besar dari Bilangan 1 dan 3")
+elif (bilangan3 > bilangan1) and (bilangan3 > bilangan2 ) :
+    print ("Bilangan 3 lebih besar dari Bilangan 1 dan 2")
+else :
+    print ("Semua Bilangan sama Besarnya")
